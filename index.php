972daeac0491fcbf8ce1f69fed4581c9e6367e90
Kato <root@localhost.localdomain>
2023-06-13 22:26:14 +0000

    #2 basic post

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
       align-items: center;
       justify-content: center;
       font-family: 'Indie Flower', cursive;
-	  background-color: #fff7d9;
+      background-color: #fff7d9;
     }
     
     input[type="text"] {
@@ -26,11 +26,26 @@
       background-color: #e9e9e9;
       cursor: pointer;
     }
+    
+    img.logo {
+      height: 100px;
+    }
   </style>
+  <script>
+    async function postData() {
+      const inputText = document.getElementById('myInput').value;
+      const response = await fetch('https://kpos.uk/deal/write/', {
+        method: 'POST',
+        body: new URLSearchParams({ inputText })
+      });
+      const responseData = await response.text();
+      console.log(responseData);
+    }
+  </script>
 </head>
 <body>
-  <img src="logo.png" alt="Logo" height="100px">
-  <input type="text" placeholder="Enter your text">
-  <button>Click Me</button>
+  <img src="logo.png" alt="Logo" class="logo">
+  <input type="text" id="myInput" placeholder="Enter your text">
+  <button onclick="postData()">Click Me</button>
 </body>
 </html>
