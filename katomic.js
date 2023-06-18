d697e92012a977b0963febcf0416ff0d9a858b1d
Kato <root@localhost.localdomain>
2023-06-18 12:32:42 +0000

    #5 copy button

--- katomic.js
+++ katomic.js
@@ -262,6 +262,16 @@ for(let i = 0; i < detectionFuncs.length; i++) {
 	// └─┘└─┘┴   ┴   ┴  └─┘┴└─┴ ┴┴ ┴┴─┘┴┘└┘┴ ┴
 	// copy permalink
 
+	// copy button
+	document.getElementById('btnCopyPermalink').innerHTML = getCopyIconSVG() // + ' Copy Permalink';
+
+	function getCopyIconSVG() {
+		return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="18px" height="18px">
+		  <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+		  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+		</svg>`;
+	}
+
 	document.getElementById('btnCopyPermalink').addEventListener('click', copyPermalink)
 
 	async function copyPermalink() {
@@ -304,25 +314,6 @@ for(let i = 0; i < detectionFuncs.length; i++) {
 	  
     }
 
-
-	document.getElementById('btnCopyPermalink').innerHTML = getCopyIconSVG() // + ' Copy Permalink';
-
-
-	function getCopyIconSVG() {
-		return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="18px" height="18px">
-		  <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-		  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-		</svg>`;
-	}
-
-
-	function getCopyIconSVGxx() {
-		
-		return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-		  <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-		  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-		</svg>`;
-	}
 
 
 	//placeholder.. templates
